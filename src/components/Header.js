6d4c44d3c1ae0dfff63d24816c0a0d9a1c5ddb2f
Shivam0069 <shivamsinghchauhan7000@gmail.com>
2024-02-01 00:51:53 +0530

    Added the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>
-      Header
+    <div className="sticky bg-white h-16 max-w-7xl  mx-auto flex items-center border-b  ">
+      <div className="h-[33px] w-full flex items-center justify-between ">
+        <div className="h-[33px] w-[115px]">
+          <Link to="/">
+            <img
+              className="w-full h-full object-cover"
+              src="https://png.pngtree.com/png-clipart/20230221/original/pngtree-cinema-ticket-icon-in-flat-style-admit-one-coupon-entrance-vector-png-image_8961928.png"
+              alt="logo"
+            />
+          </Link>
+        </div>
+       
+          <button className="mr-2 text-sm bg-[#F84464] text-white px-3 py-1 rounded-[4px] hover:brightness-110 hover:scale-110">Sign in</button>
+        
+      </div>
     </div>
-  )
+  );
 }
